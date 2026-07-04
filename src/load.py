1e21e9456752d191e3432e0103e048bde9a74138
Maciej Rak <raczek.maciek@gmail.com>
2026-07-04 23:18:13 +0200

    Add output directory parameter to save_to_csv

--- src/load.py
+++ src/load.py
@@ -1,8 +1,13 @@
 import logging
+import pandas as pd
+import pathlib
 
 logger = logging.getLogger(__name__)
 
-def save_to_csv(dataframes: dict):
+def save_to_csv(dataframes: dict[str, pd.DataFrame], output_dir: str = 'output'):
+    path = pathlib.Path(output_dir)
+    path.mkdir(parents=True, exist_ok=True)
     for name, df in dataframes.items():
-        logger.info(f"Saving {name} dataframe to {name}.csv")
-        df.to_csv(name + '.csv', index=False)
+        file_path = path / f'{name}.csv'
+        logger.info(f"Saving {name} dataframe to {file_path}")
+        df.to_csv(file_path, index=False)
