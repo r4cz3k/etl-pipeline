95f8c8fbc28b1334bc26a6b2ba77b6f39b5cbd30
Maciej Rak <raczek.maciek@gmail.com>
2026-07-06 20:14:58 +0200

    Add PostgreSQL target with connection health check

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 import src.extract as extract
 import src.transform as transform
 import src.load as load
-
+import src.config as config
+from sqlalchemy import create_engine
 
 def main() -> None:
     logging.basicConfig(
@@ -18,11 +19,22 @@ def main() -> None:
     load_dotenv()
 
     api_url = os.getenv('API_URL')
-
     if api_url is None:
         sys.exit("There was a problem loading .env file, check if .env file or API_URL variable exists")
     else:
         logger.info(".env file successfully loaded")
+
+    save_destination = os.getenv("SAVE_DESTINATION")
+    if save_destination is None:
+        sys.exit('Please set SAVE_DESTINATION environment variable')
+    elif save_destination == 'DB':
+        engine = create_engine(config.get_connection_string())
+        try:
+            with engine.connect():
+                pass # healthcheck - fail fast - if database connection fails - pipeline won't start
+        except Exception as e:
+            logger.error(f"Database connection error: {e}")
+            raise
 
     # Get data from API
     raw_data = extract.get_data(api_url)
@@ -30,8 +42,14 @@ def main() -> None:
     # Get transformed dataframes
     dataframes = transform.transform_all(raw_data)
 
-    # Save to CSV dict(filename : dataframe)
-    load.save_to_csv(dataframes)
+    # Save to CSV dict(filename : dataframe) OR DB
+    match save_destination:
+        case "DB":
+            load.save_to_postgres(dataframes, engine)
+        case "CSV":
+            load.save_to_csv(dataframes)
+        case _:
+            sys.exit(f"Unknown SAVE_DESTINATION: {save_destination}, use CSV or DB")
 
 if __name__ == '__main__':
     main()
--- src/load.py
+++ src/load.py
@@ -1,13 +1,19 @@
 import logging
 import pandas as pd
 import pathlib
+from sqlalchemy import Engine
 
 logger = logging.getLogger(__name__)
 
-def save_to_csv(dataframes: dict[str, pd.DataFrame], output_dir: str = 'output'):
+def save_to_csv(dataframes: dict[str, pd.DataFrame], output_dir: str = 'output') -> None:
     path = pathlib.Path(output_dir)
     path.mkdir(parents=True, exist_ok=True)
     for name, df in dataframes.items():
         file_path = path / f'{name}.csv'
         logger.info(f"Saving {name} dataframe to {file_path}")
         df.to_csv(file_path, index=False)
+
+def save_to_postgres(dataframes: dict[str, pd.DataFrame], engine: Engine) -> None:
+    for name, df in dataframes.items():
+        logger.info(f"Saving {name} dataframe to database")
+        df.to_sql(name, con=engine, if_exists='replace', index=False)
